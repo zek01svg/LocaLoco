6a6e632070b07a7fd173861c1cf1a974a8c14e53
zek01svg <cia.velasco.2024@smu.edu.sg>
2025-10-25 23:23:33 +0800

    chore: changed the outdir for the migration scripts

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'drizzle-kit';
 import dotenv from 'dotenv'
 
 export default defineConfig({
-    out: './src/db',
-    schema: './src/db/schema.ts',
+    out: './src/drizzle',
+    schema: './src/database/schema.ts',
     dialect: 'mysql',
     dbCredentials: {
         url: process.env.DATABASE_URL!,
