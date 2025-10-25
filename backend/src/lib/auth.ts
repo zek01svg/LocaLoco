1ee54c9fff0e5bfcaa12017b9f994e5f5864925d
zek01svg <cia.velasco.2024@smu.edu.sg>
2025-10-25 23:30:10 +0800

    feat: modified where necessary to handle google login, etc

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -1,18 +1,29 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "../database/db.js";
+import { user, session, account, verification } from "../database/schema.js";
+import dotenv from 'dotenv'
 
-export const auth = betterAuth({
+const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "mysql", 
+        schema: { user, session, account, verification }
     }),
     emailAndPassword: { 
         enabled: true, 
+        autoSignIn: true
     }, 
+    trustedOrigins: [
+        "http://localhost:3000", // backend
+        "http://localhost:5173"  // frontend
+    ],
     socialProviders: { 
         google: { 
+            prompt: "select_account",
             clientId: process.env.GOOGLE_CLIENT_ID as string, 
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
         }
     }
 });
+
+export default auth
