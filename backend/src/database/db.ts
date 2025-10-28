9139fed274b03ed7267fd8d466701f067491919d
zek01svg <cia.velasco.2024@smu.edu.sg>
2025-10-28 17:10:16 +0800

    test: added a simple script to test the connection

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,14 +1,27 @@
+import dotenv from 'dotenv'
 import mysql from 'mysql2/promise'
 import { drizzle } from 'drizzle-orm/mysql2'
-import 'dotenv/config'
 
 const db = drizzle(mysql.createPool({
-    host: process.env.DB_HOST!,
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_NAME!,
-    port: Number(process.env.DB_PORT)!
-}))
+    host: String(process.env.DB_HOST),
+    user: String(process.env.DB_USER),
+    password: String(process.env.DB_PASSWORD),
+    database: String(process.env.DB_NAME),
+    port: Number(process.env.DB_PORT),
+    ssl: {
+        // ca: fs.readFileSync(String(process.env.SSL_PATH))
+        rejectUnauthorized: false
+    }
+}));
 
+// test for the connection
+(async () => {
+    try {
+        const result = await db.execute(`SELECT 1 + 1 AS test`);
+        console.log('✅ Database connection successful! Test result:', result[0]);
+    } catch (err) {
+        console.error('❌ Database connection failed:', err);
+    }
+})();
 
 export default db
