13ace0d18c0a34f7dcb66de4cf0b71c07d94c7de
zek01svg <cia.velasco.2024@smu.edu.sg>
2025-11-01 16:23:43 +0800

    fix: fixed the login/signup/google signup by hardcoding the baseURL

---
+++ frontend2/src/lib/login-signup.ts
@@ -0,0 +1,60 @@
+import { createAuthClient } from "better-auth/client";
+
+const baseURL = 'http://localhost:3000'
+const authClient = createAuthClient({
+    baseURL: baseURL
+});
+
+document.addEventListener('DOMContentLoaded', (()=>{
+    // get the buttons 
+    const signupBtn = document.getElementById("signupBtn") as HTMLButtonElement;
+    const loginBtn = document.getElementById("loginBtn") as HTMLButtonElement;
+    const googleBtn = document.getElementById('googleBtn') as HTMLButtonElement
+
+    signupBtn.addEventListener("click", async () => {
+        const email = (document.getElementById("signupEmail") as HTMLInputElement).value;
+        const password = (document.getElementById("signupPassword") as HTMLInputElement).value;
+        const name = email.split("@")[0] as string
+
+        try {
+            const { data, error } = await authClient.signUp.email({ 
+                email, name, password,
+                callbackURL: baseURL
+            });
+            console.log({ data, error });
+        } catch (err) {
+            console.error(err);
+        }
+    });
+
+    loginBtn.addEventListener("click", async () => {
+
+        const email = (document.getElementById("loginEmail") as HTMLInputElement).value;
+        const password = (document.getElementById("loginPassword") as HTMLInputElement).value;
+
+        try {
+            const { data, error } = await authClient.signIn.email({ 
+                email, password,
+                callbackURL: baseURL
+            });
+            console.log({ data, error });
+        } catch (err) {
+            console.error(err);
+        }
+
+        console.log('login button clicked')
+    });
+
+    googleBtn.addEventListener("click", async () => {
+        try {
+            const { data, error } = await authClient.signIn.social({ 
+                provider: "google",
+                callbackURL: baseURL
+            });
+            console.log({ data, error });
+        } 
+        catch (err) {
+            console.error(err);
+        }
+    });
+}))
