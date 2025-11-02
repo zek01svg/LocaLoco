5f50f4369cd8a7d5fd8b49d0e1d4865c72754304
zek01svg <cia.velasco.2024@smu.edu.sg>
2025-11-03 02:56:04 +0800

    minor: updated to match latest db updates

--- backend/src/database/relations.ts
+++ backend/src/database/relations.ts
@@ -1,124 +1,137 @@
 import { relations } from "drizzle-orm/relations";
-import { user, account, businesses, businessOpeningHours, businessPaymentOptions, businessReviews, forumPosts, forumPostsReplies, referrals, session, vouchers, businessAnnouncements } from "./schema.js";
+import { user, account, bookmarkedBusinesses, businesses, businessAnnouncements, businessOpeningHours, businessPaymentOptions, businessReviews, forumPosts, forumPostsReplies, referrals, session, vouchers } from "./schema.js";
 
 export const accountRelations = relations(account, ({one}) => ({
-    user: one(user, {
-        fields: [account.userId],
-        references: [user.id]
-    }),
+	user: one(user, {
+		fields: [account.userId],
+		references: [user.id]
+	}),
 }));
 
 export const userRelations = relations(user, ({many}) => ({
-    accounts: many(account),
-    businessReviews: many(businessReviews),
-    businesses: many(businesses),
-    forumPosts: many(forumPosts),
-    forumPostsReplies: many(forumPostsReplies),
-    referrals_referredId: many(referrals, {
-        relationName: "referrals_referredId_user_id"
-    }),
-    referrals_referrerId: many(referrals, {
-        relationName: "referrals_referrerId_user_id"
-    }),
-    sessions: many(session),
-    vouchers: many(vouchers),
+	accounts: many(account),
+	bookmarkedBusinesses: many(bookmarkedBusinesses),
+	businessReviews: many(businessReviews),
+	businesses: many(businesses),
+	forumPosts: many(forumPosts),
+	forumPostsReplies: many(forumPostsReplies),
+	referrals_referredId: many(referrals, {
+		relationName: "referrals_referredId_user_id"
+	}),
+	referrals_referrerId: many(referrals, {
+		relationName: "referrals_referrerId_user_id"
+	}),
+	sessions: many(session),
+	vouchers: many(vouchers),
 }));
 
-export const businessOpeningHoursRelations = relations(businessOpeningHours, ({one}) => ({
-    business: one(businesses, {
-        fields: [businessOpeningHours.uen],
-        references: [businesses.uen]
-    }),
-}));
-
-export const businessAnnouncementsRelations = relations(businessAnnouncements, ({one}) => ({
-    business: one(businesses, {
-        fields: [businessAnnouncements.businessUen],
-        references: [businesses.uen]
-    }),
+export const bookmarkedBusinessesRelations = relations(bookmarkedBusinesses, ({one}) => ({
+	user: one(user, {
+		fields: [bookmarkedBusinesses.userId],
+		references: [user.id]
+	}),
+	business: one(businesses, {
+		fields: [bookmarkedBusinesses.businessUen],
+		references: [businesses.uen]
+	}),
 }));
 
 export const businessesRelations = relations(businesses, ({one, many}) => ({
-    businessAnnouncements: many(businessAnnouncements),
-    businessOpeningHours: many(businessOpeningHours),
-    businessPaymentOptions: many(businessPaymentOptions),
-    businessReviews: many(businessReviews),
-    user: one(user, {
-        fields: [businesses.ownerID],
-        references: [user.id]
-    }),
-    forumPosts: many(forumPosts),
+	bookmarkedBusinesses: many(bookmarkedBusinesses),
+	businessAnnouncements: many(businessAnnouncements),
+	businessOpeningHours: many(businessOpeningHours),
+	businessPaymentOptions: many(businessPaymentOptions),
+	businessReviews: many(businessReviews),
+	user: one(user, {
+		fields: [businesses.ownerID],
+		references: [user.id]
+	}),
+	forumPosts: many(forumPosts),
+}));
+
+export const businessAnnouncementsRelations = relations(businessAnnouncements, ({one}) => ({
+	business: one(businesses, {
+		fields: [businessAnnouncements.businessUen],
+		references: [businesses.uen]
+	}),
+}));
+
+export const businessOpeningHoursRelations = relations(businessOpeningHours, ({one}) => ({
+	business: one(businesses, {
+		fields: [businessOpeningHours.uen],
+		references: [businesses.uen]
+	}),
 }));
 
 export const businessPaymentOptionsRelations = relations(businessPaymentOptions, ({one}) => ({
-    business: one(businesses, {
-        fields: [businessPaymentOptions.uen],
-        references: [businesses.uen]
-    }),
+	business: one(businesses, {
+		fields: [businessPaymentOptions.uen],
+		references: [businesses.uen]
+	}),
 }));
 
 export const businessReviewsRelations = relations(businessReviews, ({one}) => ({
-    business: one(businesses, {
-        fields: [businessReviews.uen],
-        references: [businesses.uen]
-    }),
-    user: one(user, {
-        fields: [businessReviews.userEmail],
-        references: [user.email]
-    }),
+	business: one(businesses, {
+		fields: [businessReviews.uen],
+		references: [businesses.uen]
+	}),
+	user: one(user, {
+		fields: [businessReviews.userEmail],
+		references: [user.email]
+	}),
 }));
 
 export const forumPostsRelations = relations(forumPosts, ({one, many}) => ({
-    business: one(businesses, {
-        fields: [forumPosts.uen],
-        references: [businesses.uen]
-    }),
-    user: one(user, {
-        fields: [forumPosts.userEmail],
-        references: [user.email]
-    }),
-    forumPostsReplies: many(forumPostsReplies),
+	business: one(businesses, {
+		fields: [forumPosts.uen],
+		references: [businesses.uen]
+	}),
+	user: one(user, {
+		fields: [forumPosts.userEmail],
+		references: [user.email]
+	}),
+	forumPostsReplies: many(forumPostsReplies),
 }));
 
 export const forumPostsRepliesRelations = relations(forumPostsReplies, ({one}) => ({
-    user: one(user, {
-        fields: [forumPostsReplies.userEmail],
-        references: [user.email]
-    }),
-    forumPost: one(forumPosts, {
-        fields: [forumPostsReplies.postId],
-        references: [forumPosts.id]
-    }),
+	user: one(user, {
+		fields: [forumPostsReplies.userEmail],
+		references: [user.email]
+	}),
+	forumPost: one(forumPosts, {
+		fields: [forumPostsReplies.postId],
+		references: [forumPosts.id]
+	}),
 }));
 
 export const referralsRelations = relations(referrals, ({one, many}) => ({
-    user_referredId: one(user, {
-        fields: [referrals.referredUserId],
-        references: [user.id],
-        relationName: "referrals_referredId_user_id"
-    }),
-    user_referrerId: one(user, {
-        fields: [referrals.referrerUserId],
-        references: [user.id],
-        relationName: "referrals_referrerId_user_id"
-    }),
-    vouchers: many(vouchers),
+	user_referredId: one(user, {
+		fields: [referrals.referredUserId],
+		references: [user.id],
+		relationName: "referrals_referredId_user_id"
+	}),
+	user_referrerId: one(user, {
+		fields: [referrals.referrerUserId],
+		references: [user.id],
+		relationName: "referrals_referrerId_user_id"
+	}),
+	vouchers: many(vouchers),
 }));
 
 export const sessionRelations = relations(session, ({one}) => ({
-    user: one(user, {
-        fields: [session.userId],
-        references: [user.id]
-    }),
+	user: one(user, {
+		fields: [session.userId],
+		references: [user.id]
+	}),
 }));
 
 export const vouchersRelations = relations(vouchers, ({one}) => ({
-    user: one(user, {
-        fields: [vouchers.userId],
-        references: [user.id]
-    }),
-    referral: one(referrals, {
-        fields: [vouchers.refId],
-        references: [referrals.id]
-    }),
+	referral: one(referrals, {
+		fields: [vouchers.refId],
+		references: [referrals.id]
+	}),
+	user: one(user, {
+		fields: [vouchers.userId],
+		references: [user.id]
+	}),
 }));
