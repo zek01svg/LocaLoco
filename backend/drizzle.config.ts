23d24dd27b217a79e816a7bb413d3e5840b83905
zek01svg <cia.velasco.2024@smu.edu.sg>
2025-11-05 18:36:26 +0800

    fix: added drizzle-kit as dependency

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
     out: './src/drizzle',
     schema: './src/database/schema.ts',
     dialect: 'mysql',
     dbCredentials: {
-        url: String(process.env.DB_URL),
+        url: String(process.env.DATABASE_URL),
     },
     verbose:true,
     strict:true
